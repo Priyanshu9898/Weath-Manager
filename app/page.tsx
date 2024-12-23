85bce272c1aceaca85641097d48d725037c85545
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-12-22 23:58:32 -0500

    testimonial section is added in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import HeroSection from "@/components/Landing/HeroSection";
 import Stats from "@/components/Landing/Stats";
 import FAQS from "@/components/Landing/FAQS";
+import Testimonials from "@/components/Landing/Testimonials";
+
 const page = () => {
   return (
     <div>
       <HeroSection />
       <Stats />
       <FAQS />
+      <Testimonials />
     </div>
   );
 };
